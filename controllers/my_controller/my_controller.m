b2ffc94018361a0bf915809cee2b21d393994f4d
Filip Černý <73847259+Filip-Cerny@users.noreply.github.com>
2020-12-07 18:19:02 +0100

    Add files via upload
    Added code to avoid obstacles

--- controllers/my_controller/my_controller.m
+++ controllers/my_controller/my_controller.m
@@ -7,8 +7,8 @@
 
 % uncomment the next two lines if you want to use
 % MATLAB's desktop to interact with the controller:
-%desktop;
-%keyboard;
+desktop;
+keyboard;
 
 TIME_STEP = 64;
 
@@ -23,6 +23,7 @@ motor_right_back = wb_robot_get_device('motor_right_back');
 distance_sensor = wb_robot_get_device('distance_sensor');
 
 velocity = 2;
+rotation_counter = 0;
 
 wb_motor_set_position(motor_left_front, inf);
 wb_motor_set_velocity(motor_left_front, velocity);
@@ -40,30 +41,36 @@ wb_distance_sensor_enable(distance_sensor, TIME_STEP);
 % and leave the loop when Webots signals the termination
 %
 while wb_robot_step(TIME_STEP) ~= -1
-
-  distance = wb_distance_sensor_get_value(distance_sensor);
- if distance < 10
- velocity = 0;
-wb_motor_set_velocity(motor_left_front, velocity);
-wb_motor_set_velocity(motor_left_back, velocity);
-wb_motor_set_velocity(motor_right_front, velocity);
-wb_motor_set_velocity(motor_right_back, velocity);
-end
-  disp(distance)
-  
-  my_image = wb_camera_get_image(camera);
-  imshow(my_image)
-  
-  % read the sensors, e.g.:
- 
-  % Process here sensor data, images, etc.
-
-  % send actuator commands, e.g.:
-  %  wb_motor_set_postion(motor, 10.0);
-
-  % if your code plots some graphics, it needs to flushed like this:
-  drawnow;
-
+    distance = wb_distance_sensor_get_value(distance_sensor);
+    if rotation_counter > 0
+        velocity = 2;
+        rotation_counter = rotation_counter - 1;
+        wb_motor_set_velocity(motor_right_front, -(velocity));
+        wb_motor_set_velocity(motor_right_back, -(velocity));
+    else
+        velocity = 2;
+        wb_motor_set_velocity(motor_right_front, velocity);
+        wb_motor_set_velocity(motor_right_back, velocity);
+    end
+    if distance < 50
+        rotation_counter = 26;
+    end
+    
+    disp(distance)
+    
+    my_image = wb_camera_get_image(camera);
+    imshow(my_image)
+    
+    % read the sensors, e.g.:
+    
+    % Process here sensor data, images, etc.
+    
+    % send actuator commands, e.g.:
+    %  wb_motor_set_postion(motor, 10.0);
+    
+    % if your code plots some graphics, it needs to flushed like this:
+    drawnow;
+    prev_dist = distance;
 end
 
 % cleanup code goes here: write data to files, etc.
