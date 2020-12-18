658685431d4bca274c4280b0d08fc31cf64023ca
Adam Punčochář <goodman3599@gmail.com>
2020-12-18 20:57:27 +0100

    Finished Project

--- controllers/my_controller/my_controller.m
+++ controllers/my_controller/my_controller.m
@@ -9,17 +9,18 @@
 % MATLAB's desktop to interact with the controller:
 % desktop;
 % keyboard;
-clear
 
+clear, clc
+% proměné pro spoždění
 time = 0;
 time1 = 0;
+time2 = 0;
 
 TIME_STEP = 64;
 
 % get and enable devices, e.g.:
 camera = wb_robot_get_device('camera');
 wb_camera_enable(camera, TIME_STEP);
-start_time = wb_robot_get_name;
 
 
 motor_left_front = wb_robot_get_device('motor_left_front');
@@ -27,7 +28,6 @@ motor_left_back = wb_robot_get_device('motor_left_back');
 motor_right_front = wb_robot_get_device('motor_right_front');
 motor_right_back = wb_robot_get_device('motor_right_back');
 distance_sensor = wb_robot_get_device('distance_sensor');
-twister = wb_robot_get_device('twister');
 pivot_1 = wb_robot_get_device('pivot_1');
 pivot_2 = wb_robot_get_device('pivot_2');
 pivot_3 = wb_robot_get_device('pivot_3');
@@ -41,7 +41,6 @@ right_rotation = 0;
 left_rotation = 0;
 count_rotations = 0;
 prev_distance = 128;
-
 
 Pivot_1 = -1.25;
 Pivot_2 = -1.5;
@@ -70,56 +69,15 @@ wb_motor_set_velocity(finger_A, velocity);
 wb_motor_set_velocity(finger_B, velocity);
 wb_motor_set_velocity(finger_C, velocity);
 
-%wb_motor_set_available_force(finger_A, 0.06)
-%wb_motor_set_available_force(finger_B, 0.06)
-%wb_motor_set_available_force(finger_C, 0.06)
-
-wb_motor_enable_force_feedback(finger_A, TIME_STEP);
-
 wb_distance_sensor_enable(distance_sensor, TIME_STEP);
 
 % main loop:
 % perform simulation steps of TIME_STEP milliseconds
 % and leave the loop when Webots signals the termination
-%
+
 while wb_robot_step(TIME_STEP) ~= -1
     
     distance = wb_distance_sensor_get_value(distance_sensor);
-
-    if right_rotation > 0
-        right_rotation = right_rotation - 1;
-        wb_motor_set_velocity(motor_right_front, -(velocity));
-        wb_motor_set_velocity(motor_right_back, -(velocity));
-    else
-        wb_motor_set_velocity(motor_right_front, velocity);
-        wb_motor_set_velocity(motor_right_back, velocity);
-    end
-    if left_rotation > 0
-        left_rotation = left_rotation - 1;
-        wb_motor_set_velocity(motor_left_front, -(velocity));
-        wb_motor_set_velocity(motor_left_back, -(velocity));
-    else
-        wb_motor_set_velocity(motor_left_front, velocity);
-        wb_motor_set_velocity(motor_left_back, velocity);
-    end
-    if (right_rotation == 0) && (left_rotation == 0)
-        if (distance < 70) && (distance > 30)
-            count_rotations = count_rotations + 1;
-            if count_rotations == 1
-                right_rotation = 35;
-            elseif count_rotations == 2
-                left_rotation = 70;
-            elseif count_rotations == 3
-                left_rotation = 35;
-            end
-        end
-    end
-    if (count_rotations >= 1) && (distance == prev_distance) && (right_rotation == 0) && (left_rotation == 0)
-        count_rotations = 0;
-    end
-    prev_distance = distance;
-    disp(distance)
-
     my_image = wb_camera_get_image(camera);
     [display_out,tictac_count,IndexesOfMaxes,Pixels_left, Pixels_top] = camera_vision(my_image);
    
@@ -165,7 +123,8 @@ while wb_robot_step(TIME_STEP) ~= -1
             wb_motor_set_velocity(motor_right_back, velocity);
             end
         % Stop
-            if position(1) <= Vertical_position
+       
+            if position(1) < Vertical_position
             time1 = 0;  
             wb_motor_set_velocity(motor_left_front, 0);
             wb_motor_set_velocity(motor_left_back, 0);
@@ -192,7 +151,40 @@ while wb_robot_step(TIME_STEP) ~= -1
             grabber_pos = 0.5;
             time = 0;
             end
-    
+    if tictac_count == 0
+        if right_rotation > 0
+        right_rotation = right_rotation - 1;
+        wb_motor_set_velocity(motor_right_front, -(velocity));
+        wb_motor_set_velocity(motor_right_back, -(velocity));
+        else
+        wb_motor_set_velocity(motor_right_front, velocity);
+        wb_motor_set_velocity(motor_right_back, velocity);
+        end
+        if left_rotation > 0
+            left_rotation = left_rotation - 1;
+            wb_motor_set_velocity(motor_left_front, -(velocity));
+            wb_motor_set_velocity(motor_left_back, -(velocity));
+        else
+            wb_motor_set_velocity(motor_left_front, velocity);
+            wb_motor_set_velocity(motor_left_back, velocity);
+        end
+        if (right_rotation == 0) && (left_rotation == 0)
+            if (distance < 70) && (distance > 55)
+                count_rotations = count_rotations + 1;
+                switch count_rotations
+                    case 1
+                        right_rotation = 30;
+                    case 2 
+                        left_rotation = 60;
+                    case 3
+                        left_rotation = 30;
+                end
+            end
+        end
+        if (count_rotations >= 1) && (distance == prev_distance) && (right_rotation == 0) && (left_rotation == 0)
+            count_rotations = 0;
+        end
+    end
     % if your code plots some graphics, it needs to flushed like this:
     drawnow;
 end
